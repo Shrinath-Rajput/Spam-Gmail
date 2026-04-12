e83dbad4b51e47db6792c5cf656d98829c26c77e
Shrinath Rajput <rajputshrinath349@gmail.com>
2026-04-12 13:13:49 +0530

    clean requirements for render

--- app.py
+++ app.py
@@ -4,14 +4,20 @@ import os
 
 app = Flask(__name__)
 
+# ---------------- PATH SETUP ----------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 model_path = os.path.join(BASE_DIR, "artifacts", "model.pkl")
 vectorizer_path = os.path.join(BASE_DIR, "artifacts", "preprocessing.pkl")
 
-model = pickle.load(open(model_path, "rb"))
-vectorizer = pickle.load(open(vectorizer_path, "rb"))
+# ---------------- LOAD MODEL ----------------
+with open(model_path, "rb") as f:
+    model = pickle.load(f)
 
+with open(vectorizer_path, "rb") as f:
+    vectorizer = pickle.load(f)
+
+# ---------------- ROUTES ----------------
 @app.route("/")
 def home():
     return render_template("index.html")
@@ -28,7 +34,11 @@ def predict():
     email_vec = vectorizer.transform([email_text])
     pred = model.predict(email_vec)
 
-    return jsonify({"result": "SPAM" if pred[0] == 1 else "NOT_SPAM"})
+    return jsonify({
+        "result": "SPAM" if pred[0] == 1 else "NOT_SPAM"
+    })
 
+# ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
